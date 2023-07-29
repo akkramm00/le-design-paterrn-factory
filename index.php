00101b976c2ee314c173c1a3e0e156f08775f53f
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-29 15:38:04 +0000

    add all

--- index.php
+++ index.php
@@ -4,6 +4,24 @@
   </head>
   <body>
     <?php
+// Le principe : Une classe "chef d'orchestre":
+// = Une classe factory est une classe dont le role estd'instancier d'autres classes pour une situationdonnée.
+// Grace a ce type de classes , la logique d'instanciation d'un objet se retrouve déportée et sera toujours la meme.
+
+abstract class Notification
+  {
+    protected abstract function(string $message);// tout type de notification doit implémenter une methode send()
+    public function managerNotification($message)
+    {
+      $this -> doStuff();
+      $this -> send($message);// qu'importe le type de notification, on effectuera des actoions préalables et on envoieun message
+    }
+
+    public function doStuff()
+    {
+      // on pourrait logger des informations
+    }
+  }
 
 ?> 
 
