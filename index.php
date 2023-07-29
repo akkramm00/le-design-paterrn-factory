0d9f591b69e7f2ec0168bf2fe12ed61f9e61efef
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-29 15:27:38 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+
+?> 
+
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
